c239206ae2328bc77e03af09677e60221db059be
Daniel Gutierrez <DaniDelfin_2507@hotmail.com>
2023-02-27 01:15:59 -0600

    Ordenaminetos listos - sin pase

--- backend/ordenamineto.php
+++ backend/ordenamineto.php
@@ -9,49 +9,60 @@ $conexion = new mysqli($servidor, $usuario, $contrasena, $basedatos);
 if ($conexion->connect_error) {
     die("Connection failed: " . $conexion->connect_error);
 }
-
-//Por titulo
-$sql = "SELECT ID, Titulo, Descripcion, Categorias, Fecha FROM noticias ORDER BY Titulo";
-$result = $conexion->query($sql);
-
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo "<br> id: ". $row["ID"]. " - Titulo: ". $row["Titulo"]. " - Descripcion " . $row["Descripcion"] . " - Categoria: " . $row["Categorias"] . " - Fecha: " . $row["Fecha"] . "<br>";
-    }
-  } else {
-    echo "0 results";
-  }
   
-//Por Fecha
-$sql = "SELECT ID, Titulo, Descripcion, Categorias, Fecha FROM noticias ORDER BY Fecha";
-$result = $conexion->query($sql);
+switch($tipoDeOrdenamineto){
+    case 1: //Ordenamineto por Titulo
+        $sql = "SELECT ID, Titulo, Descripcion, Categorias, Fecha FROM noticias ORDER BY Titulo";
+        $result = $conexion->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo "<br> id: ". $row["ID"]. " - Titulo: ". $row["Titulo"]. " - Descripcion " . $row["Descripcion"] . " - Categoria: " . $row["Categorias"] . " - Fecha: " . $row["Fecha"] . "<br>";
-    }
-  } else {
-    echo "0 results";
-  }
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+            echo "<br> id: ". $row["ID"]. " - Titulo: ". $row["Titulo"]. " - Descripcion " . $row["Descripcion"] . " - Categoria: " . $row["Categorias"] . " - Fecha: " . $row["Fecha"] . "<br>";
+            }
+        } else {
+            echo "0 results";
+        }
+        break;
 
-// Por Categoria
+    case 2: //Ordenamineto por Fecha
+        $sql = "SELECT ID, Titulo, Descripcion, Categorias, Fecha FROM noticias ORDER BY Fecha";
+        $result = $conexion->query($sql);
 
-$sql = "SELECT ID, Titulo, Descripcion, Categorias, Fecha FROM noticias ORDER BY Categorias";
-$result = $conexion->query($sql);
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+              echo "<br> id: ". $row["ID"]. " - Titulo: ". $row["Titulo"]. " - Descripcion " . $row["Descripcion"] . " - Categoria: " . $row["Categorias"] . " - Fecha: " . $row["Fecha"] . "<br>";
+            }
+            } else {
+            echo "0 results";
+        }
+        break;
+    case 3: 
+        $sql = "SELECT ID, Titulo, Descripcion, Categorias, Fecha FROM noticias ORDER BY Categorias";
+        $result = $conexion->query($sql);
 
-if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-      echo "<br> id: ". $row["ID"]. " - Titulo: ". $row["Titulo"]. " - Descripcion " . $row["Descripcion"] . " - Categoria: " . $row["Categorias"] . " - Fecha: " . $row["Fecha"] . "<br>";
-    }
-  } else {
-    echo "0 results";
-  }
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+              echo "<br> id: ". $row["ID"]. " - Titulo: ". $row["Titulo"]. " - Descripcion " . $row["Descripcion"] . " - Categoria: " . $row["Categorias"] . " - Fecha: " . $row["Fecha"] . "<br>";
+            }
+            } else {
+            echo "0 results";
+        }
+        break;
+    
+    default:
+        $sql = "SELECT ID, Titulo, Descripcion, Categorias, Fecha FROM noticias ORDER BY ID";
+        $result = $conexion->query($sql);
 
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+              echo "<br> id: ". $row["ID"]. " - Titulo: ". $row["Titulo"]. " - Descripcion " . $row["Descripcion"] . " - Categoria: " . $row["Categorias"] . " - Fecha: " . $row["Fecha"] . "<br>";
+            }
+            } else {
+            echo "0 results";
+        }
+}
 
-  $conexion->close();
+$conexion->close();
 
 
 ?>
